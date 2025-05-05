09ccb7b3ca0c3177b30f848e4bda522faf12318f
Hassan Syyid <hassansyyid@gmail.com>
2025-05-05 16:30:53 -0400

    Add support for events

--- target_omnisend/sinks.py
+++ target_omnisend/sinks.py
@@ -12,7 +12,6 @@ class ContactsSink(OmnisendSink):
         state_updates = {}
 
         try:
-            # Create the payment
             response = self.request_api(
                 "POST", endpoint=self.endpoint, request_data=record
             )
@@ -26,4 +25,31 @@ class ContactsSink(OmnisendSink):
                 return None, False, {"error": f"{response.status_code} - {response.text}"}
         except Exception as e:
             self.logger.error(f"Failed to post contact: {str(e)}")
+            return None, False, {"error": str(e)}
+
+
+class EventsSink(OmnisendSink):
+    name = "events"
+    endpoint = "/events"
+    batch = None
+
+    def preprocess_record(self, record: dict, context: dict) -> dict:
+        return record
+
+    def upsert_record(self, record: dict, context: dict):
+        state_updates = {}
+
+        try:
+            response = self.request_api(
+                "POST", endpoint=self.endpoint, request_data=record
+            )
+
+            if response.ok:
+                self.logger.info(f"Successfully posted event")
+                return None, True, state_updates
+            else:
+                self.logger.error(f"Failed to post event: {response.text}")
+                return None, False, {"error": f"{response.status_code} - {response.text}"}
+        except Exception as e:
+            self.logger.error(f"Failed to post event: {str(e)}")
             return None, False, {"error": str(e)}

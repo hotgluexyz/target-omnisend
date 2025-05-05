07647c6a8f69bf05feb43e3a0dba4a9413258ef9
Hassan Syyid <hassansyyid@gmail.com>
2025-05-05 18:03:52 -0400

    Add EventsSink

--- target_omnisend/target.py
+++ target_omnisend/target.py
@@ -3,12 +3,13 @@ from target_hotglue.target import TargetHotglue
 
 from target_omnisend.sinks import (
     ContactsSink,
+    EventsSink,
 )
 
 
 class TargetOmnisend(TargetHotglue):
     name = "target-omnisend"
-    SINK_TYPES = [ContactsSink]
+    SINK_TYPES = [ContactsSink, EventsSink]
     MAX_PARALLELISM = 1
 
     config_jsonschema = th.PropertiesList(
